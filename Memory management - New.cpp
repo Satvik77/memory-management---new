705077f537986da6f128e1ad41b3ea8e8c32e75c
Satvik77 <83899207+Satvik77@users.noreply.github.com>
2021-12-17 15:54:14 +0530

    Add files via upload

---
+++ Memory management - New.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+class Test
+{
+private:
+    int data;
+public:
+     void setData(int set)
+     {
+        data = set;
+     }
+     int getData()
+     {
+         return data;
+     }            
+};
+int main()
+{
+    Test *ptr = new Test;
+    ptr->setData(10);
+    cout << "Value is : " << ptr->getData();
+    
+    return 0;
+
+}
